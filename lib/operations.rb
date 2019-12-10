3114b4251e7eea4309187fff2c0e384317bf797f
Randall Williams <randallw1996@gmail.com>
2019-12-10 03:58:03 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,7 +11,7 @@ end
 
 
 def not_safe?(speed)
- speed > 60 || 40 < speed ? true : false unless
+ speed > 60 || 40 < speed ? true : false
  #speed < 40 ? true : false
   # 40 < speed and speed > 60 ? true : false 
   # speed < 40 ? true : false
